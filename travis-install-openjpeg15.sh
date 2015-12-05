2ebbd61cbc5170e449b5e170e17f70421aa58462
John Evans <john.g.evans.ne@gmail.com>
2015-12-05 07:13:32 -0500

    Remove py27 jobs for numpy 1.8 and 1.9
    Those two jobs don't give us much.
    Fix tests for warnings on py27 that were broken in issue 361

--- travis-install-openjpeg15.sh
+++ travis-install-openjpeg15.sh
@@ -5,8 +5,8 @@
 set -e
 
 # Compile openjpeg from source, install into home directory.
-wget https://github.com/uclouvain/openjpeg/archive/openjpeg-version.1.5.2.tar.gz
-tar xvfz openjpeg-version.1.5.2.tar.gz
+wget https://github.com/uclouvain/openjpeg/archive/version.1.5.2.tar.gz
+tar xvfz version.1.5.2.tar.gz
 mkdir -p openjpeg-version.1.5.2/build
 cd openjpeg-version.1.5.2/build && cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/openjpeg && make && make install
 

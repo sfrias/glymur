f9a239cb47e656fa06cfed17949fbabc5669e870
John Evans <john.g.evans.ne@gmail.com>
2016-12-27 19:28:17 -0500

    Have conda install from job-specific files

--- ci/install_travis.sh
+++ ci/install_travis.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 conda config --append channels conda-forge
-conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION numpy contextlib2 mock openjpeg
+conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION
 source activate test-environment
+conda install --file ci/"$JOB_NAME".txt

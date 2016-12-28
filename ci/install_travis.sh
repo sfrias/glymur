dbd381f38fe834ece34a9f15f771e193351d3058
John Evans <john.g.evans.ne@gmail.com>
2016-12-27 19:35:18 -0500

    Swap JOB_NAME with JOB_TAG environment variable

--- ci/install_travis.sh
+++ ci/install_travis.sh
@@ -2,4 +2,4 @@
 conda config --append channels conda-forge
 conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION
 source activate test-environment
-conda install --file ci/"$JOB_NAME".txt
+conda install --file ci/"$JOB_TAG".txt

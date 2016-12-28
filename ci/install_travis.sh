d86ef8a77ce75353c7a831fbb53191f0a9277c24
John Evans <john.g.evans.ne@gmail.com>
2016-12-27 21:21:25 -0500

    Fix lxml to 3.6.4

--- ci/install_travis.sh
+++ ci/install_travis.sh
@@ -1,5 +1,1 @@
 #!/bin/bash
-conda config --append channels conda-forge
-conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION
-source activate test-environment
-conda install --file ci/"$JOB_TAG".txt
